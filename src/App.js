6a715d7a6a42a8d0d8f0b455c981c454b3b2384e
Suivez <suivez@interia.pl>
2024-06-25 23:10:57 +0200

    25.06

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
           <div className='learning-box'>
             Learning
           </div>
+          <p>Published 21 Dec 2023</p>
+          <h2>HTML & CSS foundations</h2>
+          <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
         </div>
       </div>
     </div>
